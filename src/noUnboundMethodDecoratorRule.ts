31c7ff1e0e34980380b0f05816c14b799f8ea928
Eric Gross <eric.gross@orum.com>
2022-11-01 13:40:43 -0700

    Add support decorators that take arguments

--- src/noUnboundMethodDecoratorRule.ts
+++ src/noUnboundMethodDecoratorRule.ts
@@ -212,9 +212,8 @@ function isDecorated(node: ts.Node, decorator: string): boolean {
         return false
       }
       const res = node.decorators.some(d => {
-        return (
-          isIdentifier(d.expression) && d.expression.escapedText === decorator
-        )
+        const expression = getExpression(d)
+        return isIdentifier(expression) && expression.escapedText === decorator
       })
       return res
     default:
@@ -272,4 +271,12 @@ function isWhitelisted(
     return whitelist.has(callingMethodName)
   }
   return false
+}
+
+function getExpression(decorator: ts.Decorator): ts.LeftHandSideExpression {
+  if (isCallExpression(decorator.expression)) {
+    return (decorator.expression as ts.CallExpression).expression
+  }
+
+  return decorator.expression
 }
